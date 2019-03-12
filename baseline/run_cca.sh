09bf03b0a8c6f8a7acb93e031fa26640142b61ec
xfbai <bxf_hit@163.com>
2019-03-12 18:05:11 +0800

    Update run_cca.sh

--- baseline/run_cca.sh
+++ baseline/run_cca.sh
@@ -5,6 +5,6 @@ datadir=/data/embeddings
 src_path=$datadir/new_embedding_size200.$src_lang
 tgt_path=$datadir/new_embedding_size200.$tgt_lang
 
-dict_path=/data/dictionaries/ACL16/dict.$tgt_lang-$src_lang.new
+dict_path=data/bldicts/dict.$tgt_lang-$src_lang.new
 
 python CCA_project_vectors.py -slang $src_lang -tlang $tgt_lang -semb $src_path -temb $tgt_path -d $dict_path
