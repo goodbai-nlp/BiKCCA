d372d2a78864b84159dee65d56a2e63d0bba3fda
xfbai <bxf_hit@163.com>
2019-03-12 18:05:38 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,6 +5,6 @@ datadir=/data/embeddings
 src_path=$datadir/new_embedding_size200.$src_lang
 tgt_path=$datadir/new_embedding_size200.$tgt_lang
 
-dict_path=/data/dictionaries/ACL16/dict.$tgt_lang-$src_lang.new
+dict_path=data/bldicts/dict.$tgt_lang-$src_lang.new
 
 python train.py -slang $src_lang -tlang $tgt_lang -semb $src_path -temb $tgt_path -d $dict_path -reg 0.3  -g1 0.001  -g2 0.001
